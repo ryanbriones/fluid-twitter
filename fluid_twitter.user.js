6303adb94fe6577b866fc801813d1b3043afc589
Ryan Briones <ryan.briones@brionesandco.com>
2008-03-04 00:35:36 -0500

    Class'ify Fluid Twitter

--- fluid_twitter.user.js
+++ fluid_twitter.user.js
@@ -8,25 +8,41 @@
 // @homepage        http://brionesandco.com/ryabriones
 // ==/UserScript==
 
-function refreshTwitter() { 
-	if($$('.wrapper').first().cumulativeScrollOffset()[1] > 90) return setTimeout(refreshTwitter, 30000);
-	if($F('status') != '') return setTimeout(refreshTwitter, 30000);
+var FluidTwitter = Class.create({
+	initialize: function() {
+		this.trimUI();
+		
+		setTimeout(this.refreshTwitter.bind(this), 30000);
+	},
 	
-	window.location.reload();
-}
+	trimUI: function() {
+	  // hide some unneccessary parts of the UI
+	  $('side', 'footer', 'navigation').invoke('hide');
+	  $$('.badge').first().hide();
+	  
+	  // alter divs to stretch with the viewport
+	  $('container', 'content').invoke('setStyle', {width: '100%'});
+	  $('status').setStyle({width: '90%'});
+
+    // alter "What are you doing?" h3 to allow thin viewport
+		$$('.bar').first().down('h3').down('label').setStyle({padding: 0});
+	  
+		$('header').down('img').setStyle({paddingLeft: '10px'});
+	},
+	
+	refreshTwitter: function() {
+	  // don't refresh if i'm scrolling
+		if($$('.wrapper').first().cumulativeScrollOffset()[1] > 90) 
+		  return setTimeout(this.refreshTwitter.bind(this), 30000);
+		
+		// don't refresh if i'm typing either
+		if($F('status') != '')
+		  return setTimeout(this.refreshTwitter.bind(this), 30000);
+    
+		window.location.reload();
+	}
+});
 
 (function() {
-	$('side').hide();
-	$('footer').hide();
-	$('navigation').hide();
-
-	$('container').setStyle({width: '100%'});
-	$('content').setStyle({width: '100%'});
-	$('header').down('img').setStyle({paddingLeft: '10px'});
-
-	$('status').setStyle({width: '90%'});
-	$$('.bar').first().down('h3').down('label').setStyle({padding: 0});
-	$$('.badge').first().hide();
-	
-	setTimeout(refreshTwitter, 30000);
+  document.fluid_twitter = new FluidTwitter();
 })();
